a8248916f95c44b1752cbc6ceff3fb186b4179ff
halgab <24685886+halgab@users.noreply.github.com>
2023-09-26 14:30:35 +0200

    annotate DesignerActionMethodItem

--- src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DesignerActionVerbItem.cs
+++ src/System.Windows.Forms.Design/src/System/Windows/Forms/Design/DesignerActionVerbItem.cs
@@ -7,7 +7,7 @@ internal class DesignerActionVerbItem : DesignerActionMethodItem
 {
     private readonly DesignerVerb _targetVerb;
 
-    public DesignerActionVerbItem(DesignerVerb verb) : base(null, null, null)
+    public DesignerActionVerbItem(DesignerVerb verb) : base(null, null!, null)
     {
         Debug.Assert(verb is not null, "All callers check whether the verb is null.");
         _targetVerb = verb;
